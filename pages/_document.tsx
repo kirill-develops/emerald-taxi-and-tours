595d937e95a144b32826cca4a890c44ded76e8af
Kirill Tchentsov <kirill.develops@gmail.com>
2023-01-31 19:49:18 -0500

    updated documet & theme.js incorporating font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
-import theme from '../material/theme';
+import theme, { roboto } from '../material/theme'; //todo add user theme fetching logic
 import createEmotionCache from '../material/createEmotionCache';
 
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className={roboto.className}>
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
