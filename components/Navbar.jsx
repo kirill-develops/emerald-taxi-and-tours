a4055b00e086065d0b0e673dc4804fdee0eecf6a
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-06 15:15:03 -0500

    created Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+import Link from '../material/Link';
+import MobilePagesMenu from './NavbarComponents/MobilePagesMenu';
+import MobileSearchModal from './NavbarComponents/MobileSearchModal';
+import TabletPagesLink from './NavbarComponents/TabletPagesLink';
+
+export const pages = [
+  { name: 'Airport Transfer', link: 'transfer' },
+  { name: 'Tours', link: 'tours' },
+  { name: 'About Us', link: 'about' },
+];
+
+function Navbar() {
+  const menuBreakpoint = 'md';
+
+  return (
+    <AppBar
+      sx={{
+        position: { xs: 'fixed', sm: 'initial' },
+        top: { xs: 'auto', sm: 'initial' },
+        bottom: { xs: 0, sm: 'initial' },
+      }}
+    >
+      <Toolbar>
+        <MobilePagesMenu dissapearingBreakpoint={menuBreakpoint} />
+        <Typography
+          variant="h6"
+          component={Link}
+          href="/"
+          noWrap
+          sx={{
+            flexGrow: 1,
+            color: 'inherit',
+            textDecoration: 'none',
+            textAlign: { xs: 'center', md: 'initial' },
+          }}
+        >
+          Emerald Taxi & Tours
+        </Typography>
+        <TabletPagesLink dissapearingBreakpoint={menuBreakpoint} />
+        <MobileSearchModal dissapearingBreakpoint={menuBreakpoint} />
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
