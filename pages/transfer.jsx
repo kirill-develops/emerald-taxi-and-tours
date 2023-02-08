a7f29fbba9543c261aade5563f6fe684139ec9db
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-08 14:15:37 -0500

    created hook to render destination accordion array for each airport

--- pages/transfer.jsx
+++ pages/transfer.jsx
@@ -1,12 +1,49 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
+import DestinationAccordion from '../components/TransfersComponents/DestinationAccordion';
+import {
+  normanManleyTransferData,
+  sangsterTransferData,
+} from '../data/transfers';
+
+function useAccordionElArray(transferData) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (_event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return transferData.map(({ name, link, destinations }) => {
+    return (
+      <DestinationAccordion
+        key={link}
+        name={name}
+        link={link}
+        destinations={destinations}
+        expanded={expanded}
+        handleChange={handleChange}
+      />
+    );
+  });
+}
 
 function Transfer() {
+  const sangsterAccordionElArray = useAccordionElArray(sangsterTransferData);
+
+  const normanManleyAccordionElArray = useAccordionElArray(
+    normanManleyTransferData,
+  );
+
   return (
     <main>
       <Layout>
-        <Typography>Transfers</Typography>
+        <Typography>AIRPORT TRANSFERS</Typography>
+        <Typography>Sangster International, Montego Bay</Typography>
+        <Container maxWidth="lg">{sangsterAccordionElArray}</Container>
+        <Typography>Norman Manley International, Kingston</Typography>
+        <Container maxWidth="lg">{normanManleyAccordionElArray}</Container>
       </Layout>
     </main>
   );
