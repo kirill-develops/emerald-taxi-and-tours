0cadaeb3abd6ea0c5cead5879d82595c381e7fd3
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-08 14:01:49 -0500

    created basic card representing location data

---
+++ components/TransfersComponents/AccordionCard.jsx
@@ -0,0 +1,98 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginRight: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const cardStyleProps = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+};
+
+function getSubheader(linkEnding, destination, area) {
+  switch (linkEnding) {
+    case 'other':
+      return destination.area;
+    case 'norman_manley':
+      return destination.parish;
+    default:
+      return area;
+  }
+}
+
+function AccordionCard({ destination, area, linkEnding }) {
+  const { name, link, price } = destination;
+  const destinationURL = `${link}_${linkEnding}`;
+
+  const subheader = getSubheader(linkEnding, destination, area);
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Grid
+      item
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+    >
+      <Card
+        variant="outlined"
+        key={destinationURL}
+        sx={cardStyleProps}
+      >
+        <CardHeader
+          title={name}
+          subheader={subheader}
+        />
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+          <Button variant="contained">Book Now</Button>
+        </CardActions>
+        <Collapse
+          in={expanded}
+          timeout="auto"
+          unmountOnExit
+        >
+          <CardContent>
+            <Typography>One Way: ${price.oneWay}</Typography>
+            <Typography>Round Trip: ${price.roundTrip}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  );
+}
+
+export default AccordionCard;
