6a2917f92c2a74931b4afba6f6f4dcd5a5ba1d66
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-10 14:12:52 -0500

    refactored sticky navBar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,17 @@
 import Container from '@mui/material/Container';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { styled, useTheme } from '@mui/material/styles';
 import React, { useEffect, useState } from 'react';
 import Copyright from './Copyright';
 import Navbar from './Navbar';
 
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+
 function Layout({ children }) {
   const [isMounted, setIsMounted] = useState(false);
+
+  const theme = useTheme();
+  const isXsBreakpoint = useMediaQuery(theme.breakpoints.only('xs'));
 
   useEffect(() => setIsMounted(true), []);
 
@@ -13,8 +20,18 @@ function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <Container maxWidth="xl">{children}</Container>
-      <Copyright />
+      <Container
+        maxWidth="xl"
+        sx={{
+          height: { sm: '91vh' },
+          minHeight: { sm: '100%' },
+          overflow: { sm: 'auto' },
+        }}
+      >
+        {children}
+        <Copyright sx={{ overflow: 'auto' }} />
+        {isXsBreakpoint && <Offset />}
+      </Container>
     </>
   );
 }
