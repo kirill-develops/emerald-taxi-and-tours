86161bce519412bc8374b5d3433feeea6eeddd55
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-12 12:14:41 -0500

    added store to Sort & TourGrid components; minor style changes

--- components/ToursComponents/Sort.jsx
+++ components/ToursComponents/Sort.jsx
@@ -1,11 +1,15 @@
+import SortOutlined from '@mui/icons-material/SortOutlined';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import React, { useState } from 'react';
+import React from 'react';
+import { useTour } from '../../state/useSort';
 
 function Sort() {
-  const [sort, setSort] = useState('');
+  const [state, actions] = useTour();
+  const { sort } = state;
+  const { setSort } = actions;
 
   const handleSortChange = (e) => {
     setSort(e.target.value);
@@ -20,12 +24,22 @@ function Sort() {
   return (
     <FormControl
       onKeyUp={handleClearSelect}
-      sx={{ m: 1, width: { xs: '50%', sm: '25%' } }}
+      sx={{ width: { xs: '40%', sm: '25%' } }}
       size={'small'}
     >
-      <InputLabel>Sort</InputLabel>
+      <InputLabel
+        sx={{
+          width: '45%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        Sort
+        <SortOutlined />
+      </InputLabel>
       <Select
-        label="Sort"
+        label="Sort......."
         value={sort}
         onChange={handleSortChange}
         sx={{ backgroundColor: 'white' }}
--- components/ToursComponents/TourGrid.jsx
+++ components/ToursComponents/TourGrid.jsx
@@ -1,27 +1,40 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Container from '@mui/material/Container';
-import Collapse from '@mui/material/Collapse';
 import Grid from '@mui/material/Grid';
-import React, { useState } from 'react';
-import { cardStyleProps, gridSpacingProps } from '../../material/theme';
+import React from 'react';
+import { gridSpacingProps } from '../../material/theme';
 import { tourData } from '../../data/tours';
-import ExpandMore from '../ExpandMore';
-import PriceTable from './PriceTable';
-import DestinationType from './DestinationType';
+import { useTour } from '../../state/useSort';
+import TourCard from './TourCard';
+
+function useSortData(data, sortBy = '') {
+  const clonedData = structuredClone(data);
+  switch (sortBy) {
+    case '':
+      return clonedData;
+    case 'alphabetical':
+      return clonedData.sort((a, b) => (a.name < b.name ? -1 : 1));
+    case 'priceAscending':
+      return clonedData; //! figure out sort
+    case 'priceDescending':
+      return clonedData; //! figure out sort
+    case 'region':
+      return clonedData.sort((a, b) => (a.area < b.area ? -1 : 1));
+  }
+}
 
 function TourGrid() {
+  const [state, actions] = useTour();
+  const { sort } = state;
+
+  const sortedTourData = useSortData(tourData, sort);
+
   return (
     <Container maxWidth="lg">
       <Grid
         container
         spacing={gridSpacingProps}
       >
-        {tourData.map((tour) => (
+        {sortedTourData.map((tour) => (
           <TourCard
             tour={tour}
             key={tour.link}
@@ -31,54 +44,4 @@ function TourGrid() {
     </Container>
   );
 }
-
-const gridItemProps = { xs: 12, md: 6, lg: 4, item: true };
-
-function TourCard({ tour }) {
-  const { name, area, link, type, price } = tour;
-
-  const [expanded, setExpanded] = useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Grid {...gridItemProps}>
-      <Card
-        variant="outlined"
-        sx={cardStyleProps}
-      >
-        <CardHeader
-          title={name}
-          subheader={area}
-        />
-        <CardContent>
-          <DestinationType typeArr={type} />
-        </CardContent>
-        <CardActions disableSpacing>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-          <Button variant="contained">Book Now</Button>
-        </CardActions>
-        <Collapse
-          in={expanded}
-          timeout="auto"
-          unmountOnExit
-        >
-          <CardContent>
-            <PriceTable pricesArr={price} />
-          </CardContent>
-        </Collapse>
-      </Card>
-    </Grid>
-  );
-}
-
 export default TourGrid;
