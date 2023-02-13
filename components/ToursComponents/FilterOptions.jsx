36044063d0c31e6d2d9eab6fd37414e5d91765b4
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-12 20:54:06 -0500

    created parent Component to house filter options Components

---
+++ components/ToursComponents/FilterOptions.jsx
@@ -0,0 +1,22 @@
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import AttractionRegion from '../FilterOptionComponents/AttractionRegion';
+import AttractionTypes from '../FilterOptionComponents/AttractionTypes';
+import StartingLocations from '../FilterOptionComponents/StartingLocations';
+
+function FilterOptions() {
+  return (
+    <Card sx={{ m: 2, p: 1 }}>
+      <Typography>Starting Location</Typography>
+      <StartingLocations />
+      <Typography>Attraction Type</Typography>
+      <AttractionTypes />
+      <Typography>Attraction Region</Typography>
+      <AttractionRegion />
+      <Typography>Price Range</Typography>
+    </Card>
+  );
+}
+
+export default FilterOptions;
