936544b46526658e99692c13d32313cca77cdede
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-12 21:09:33 -0500

    created function to filter data with selected filters from store

--- components/ToursComponents/TourGrid.jsx
+++ components/ToursComponents/TourGrid.jsx
@@ -6,27 +6,50 @@ import { tourData } from '../../data/tours';
 import { useTour } from '../../state/useSort';
 import TourCard from './TourCard';
 
+function useFilterData(data, startLocationFilter, typeFilter, areaFilter) {
+  const filteredData = data.filter((item) => {
+    let include = false;
+    if (areaFilter[item.area]) {
+      include = true;
+    } else if (item.type.some((t) => typeFilter[t])) {
+      include = true;
+    } else if (item.price.some((p) => startLocationFilter[p.link])) {
+      include = true;
+    }
+    return include;
+  });
+
+  return filteredData.length > 0 ? filteredData : data;
+}
+
 function useSortData(data, sortBy = '') {
   const clonedData = structuredClone(data);
   switch (sortBy) {
     case '':
       return clonedData;
     case 'alphabetical':
-      return clonedData.sort((a, b) => (a.name < b.name ? -1 : 1));
+      return clonedData.sort((a, b) => a.name.localeCompare(b.name));
     case 'priceAscending':
       return clonedData; //! figure out sort
     case 'priceDescending':
       return clonedData; //! figure out sort
     case 'region':
-      return clonedData.sort((a, b) => (a.area < b.area ? -1 : 1));
+      return clonedData.sort((a, b) => a.area.localeCompare(b.area));
   }
 }
 
 function TourGrid() {
   const [state, actions] = useTour();
-  const { sort } = state;
+  const { sort, filterStartLocation, filterType, filterArea } = state;
 
-  const sortedTourData = useSortData(tourData, sort);
+  const filteredTourData = useFilterData(
+    tourData,
+    filterStartLocation,
+    filterType,
+    filterArea,
+  );
+
+  const sortedTourData = useSortData(filteredTourData, sort);
 
   return (
     <Container maxWidth="lg">
