9c87528cca0e6408cc3db18368b84fe397abdb09
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-21 20:02:29 -0500

    refactored linkComponent to prevent page reload

--- components/ToursComponents/TourCard.jsx
+++ components/ToursComponents/TourCard.jsx
@@ -14,6 +14,7 @@ import PriceTable from './PriceTable';
 import TourType from './TourType';
 import PickUpCardHeader from './PickUpCardHeader';
 import { GridItem } from '@elements/CustomGridEl';
+import Link from '@material/Link';
 
 function Description({ description }) {
   return description.length > 0 && <Typography>{description}</Typography>;
@@ -55,6 +56,7 @@ function TourCard({ tour }) {
         >
           <Button
             variant="contained"
+            LinkComponent={Link}
             href={`../tours/${link}`}
           >
             Book Now
