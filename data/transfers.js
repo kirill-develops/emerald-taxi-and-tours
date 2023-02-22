ee832287bdb69d8f71e2dc7192f52c1e8d521994
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-22 16:49:01 -0500

    added airportLink prop

--- data/transfers.js
+++ data/transfers.js
@@ -1,6 +1,6 @@
 export const sangsterTransferData = [
   {
-    name: 'Montego Bay', link: 'montego_bay', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Montego Bay', link: 'montego_bay', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 10, roundTrip: 20 } },
       { name: "Breathless", link: 'breathless', price: { oneWay: 9, roundTrip: 16 } },
       { name: "Half Moon", link: 'half_moon', price: { oneWay: 15, roundTrip: 25 } },
@@ -21,7 +21,7 @@ export const sangsterTransferData = [
     ]
   },
   {
-    name: 'Falmouth', link: 'falmouth', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Falmouth', link: 'falmouth', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 17.5, roundTrip: 30 } },
       { name: "Excellence, Oyster Bay", link: 'excellence_oyster_bay', price: { oneWay: 15, roundTrip: 25 } },
       { name: "Ocean Coral Springs by H10 Hotels", link: 'ocean_coral', price: { oneWay: 17.5, roundTrip: 32.5 } },
@@ -29,13 +29,13 @@ export const sangsterTransferData = [
     ]
   },
   {
-    name: 'Lucea', link: 'lucea', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Lucea', link: 'lucea', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 15, roundTrip: 25 } },
       { name: "Grand Palladium & Lady Hamilton, Lucea", link: 'grand_palladium', price: { oneWay: 15, roundTrip: 25 } },
     ]
   },
   {
-    name: 'Negril', link: 'negril', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Negril', link: 'negril', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 25, roundTrip: 45 } },
       { name: "Azul Beach Resort, Negril", link: 'azul_beach_resort', price: { oneWay: 25, roundTrip: 45 } },
       { name: "Couples Swept Away & Couples Negril", link: 'couples', price: { oneWay: 25, roundTrip: 45 } },
@@ -47,12 +47,12 @@ export const sangsterTransferData = [
       { name: "Sunset at the Palms Resort", link: 'sunset_palms', price: { oneWay: 25, roundTrip: 45 } },
     ]
   }, {
-    name: 'Braco', link: 'braco', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Braco', link: 'braco', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 20, roundTrip: 35 } },
       { name: "Melia Braco Village", link: 'melia_braco_village', price: { oneWay: 20, roundTrip: 35 } },
     ]
   }, {
-    name: 'Runaway Bay', link: 'runaway_bay', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Runaway Bay', link: 'runaway_bay', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 22.5, roundTrip: 40 } },
       { name: "Bahia Principe Grand & Luxury, Runaway Bay", link: 'bahia_principe', price: { oneWay: 22.5, roundTrip: 40 } },
       { name: "Franklyn D Resort & Spa, Runaway Bay", link: 'franklyn_d', price: { oneWay: 22.5, roundTrip: 40 } },
@@ -61,12 +61,12 @@ export const sangsterTransferData = [
       { name: "Royal Decameron Club CaribbeanResort, Runaway Bay", link: 'royal_decameron', price: { oneWay: 22.5, roundTrip: 40 } },
     ]
   }, {
-    name: 'South Coast', link: 'south_coast', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'South Coast', link: 'south_coast', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 22.5, roundTrip: 40 } },
       { name: "Sandals Whitehouse, South Coast", link: 'sandals_whitehouse', price: { oneWay: 25, roundTrip: 35 } },
     ]
   }, {
-    name: 'Ocho Rios', link: 'ocho_rios', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Ocho Rios', link: 'ocho_rios', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes", link: 'other', price: { oneWay: 27.5, roundTrip: 50 } },
       { name: "Couple Tower Isle", link: 'couple_tower_isle', price: { oneWay: 27.5, roundTrip: 50 } },
       { name: "Couples Sans Souci All Inclusive", link: 'couples_sans_souci', price: { oneWay: 27.5, roundTrip: 50 } },
@@ -77,7 +77,7 @@ export const sangsterTransferData = [
     ]
   },
   {
-    name: 'Other Areas', link: 'other', airport: 'Sangster Internation (MBJ)', destinations: [
+    name: 'Other Areas', link: 'other', airport: 'Sangster Internation (MBJ)', airportLink: 'MBJ', destinations: [
       { name: "All Other Resorts, Villas, AirBnB & Homes in TREASURE BEACH", area: 'Treasure Beach', link: 'treasure_beach', price: { oneWay: 40, roundTrip: 75 } },
       { name: "All Other Resorts, Villas, AirBnB & Homes in MANDEVILLE", area: 'Mandeville', link: 'mandeville', price: { oneWay: 50, roundTrip: 80 } },
       { name: "All Other Resorts, Villas, AirBnB & Homes in PORT ANTONIO", area: 'Port Antonio', link: 'port_antonio', price: { oneWay: 90, roundTrip: 160 } },
@@ -88,7 +88,7 @@ export const sangsterTransferData = [
 
 export const normanManleyTransferData = [
   {
-    name: 'All Regions', link: 'norman_manley', airport: 'Norman Manley (KIN)', destinations: [
+    name: 'All Regions', link: 'norman_manley', airport: 'Norman Manley (KIN)', airportLink: 'KIN', destinations: [
       { name: 'Kingston, Spanish Town & Portmore', link: 'kingston_area', price: { oneWay: 20, roundTrip: 30 } },
       { name: 'Falmouth & Duncans', parish: 'Trelawny', link: 'trelawny', price: { oneWay: 70, roundTrip: 125 } },
       { name: 'Lucea', parish: 'Hanover', link: 'lucea', price: { oneWay: 80, roundTrip: 150 } },
