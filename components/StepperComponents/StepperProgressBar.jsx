deae344307d04f3497db6e2f0095a0622bbffde9
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-26 13:58:35 -0500

    refactored to use fn extracting transfer labels

--- components/StepperComponents/StepperProgressBar.jsx
+++ components/StepperComponents/StepperProgressBar.jsx
@@ -3,15 +3,17 @@ import StepLabel from '@mui/material/StepLabel';
 import Stepper from '@mui/material/Stepper';
 import React from 'react';
 
-import { transferSteps } from '@data/transferFormData';
+import { useTransferDataByKey } from '@data/transferFormData';
 
 function StepperProgressBar({ activeStep }) {
+  const transferLabels = useTransferDataByKey('label');
+
   return (
     <Stepper
       activeStep={activeStep}
       alternativeLabel
     >
-      {transferSteps.map((label, index) => (
+      {transferLabels.map(({ label }, index) => (
         <Step key={label}>
           <StepLabel>{activeStep >= index && label}</StepLabel>
         </Step>
