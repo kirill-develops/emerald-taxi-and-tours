3a6c278579ae277dd3f6f7bf01577cbdc0a3f0bb
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-26 17:26:23 -0500

    refactored to check if values being set to cookie are new values before setting cookie

--- hooks/useFormCookie.jsx
+++ hooks/useFormCookie.jsx
@@ -1,32 +1,37 @@
 import dayjs from 'dayjs';
+import isEqual from 'lodash/isEqual';
 import { useCallback, useMemo } from 'react';
 import { useCookies } from 'react-cookie';
 
 function useFormCookie(initialValues) {
   const [cookies, setCookie] = useCookies(['EmeraldTransferFormCache']);
   const data = cookies.EmeraldTransferFormCache;
-  const parsedData = useMemo(
-    () =>
-      data
-        ? {
-            ...initialValues,
-            ...data,
-            flightDetails: {
-              ...initialValues.flightDetails,
-              ...data.flightDetails,
-              arrive: dayjs(
-                data?.flightDetails?.arrive ||
-                  initialValues?.flightDetails?.arrive,
-              ),
-              depart: dayjs(
-                data?.flightDetails?.depart ||
-                  initialValues?.flightDetails?.depart,
-              ),
-            },
-          }
-        : initialValues,
-    [data, initialValues],
+
+  const parseFlightDetails = useCallback(
+    (data) => {
+      return {
+        ...initialValues.flightDetails,
+        ...data.flightDetails,
+        arrive: dayjs(
+          data?.flightDetails?.arrive || initialValues?.flightDetails?.arrive,
+        ),
+        depart: dayjs(
+          data?.flightDetails?.depart || initialValues?.flightDetails?.depart,
+        ),
+      };
+    },
+    [initialValues.flightDetails],
   );
+
+  const parsedData = useMemo(() => {
+    if (!data) return initialValues;
+
+    return {
+      ...initialValues,
+      ...data,
+      flightDetails: parseFlightDetails(data),
+    };
+  }, [data, initialValues, parseFlightDetails]);
 
   const setFormCookie = useCallback(
     (values) => {
@@ -34,19 +39,32 @@ function useFormCookie(initialValues) {
         path: '/',
         maxAge: 60 * 60 * 24 * 7, // 1 week
       };
-      setCookie(
-        'EmeraldTransferFormCache',
-        JSON.stringify({
-          ...data,
-          ...values,
-        }),
-        cookieOptions,
-      );
+
+      const formattedCookie = {
+        ...data,
+        flightDetails: parseFlightDetails(data),
+      };
+
+      const areValuesSame = isEqual(formattedCookie, values);
+
+      if (!areValuesSame) {
+        setCookie(
+          'EmeraldTransferFormCache',
+          JSON.stringify({
+            ...data,
+            ...values,
+          }),
+          cookieOptions,
+        );
+      }
     },
-    [data, setCookie],
+    [data, parseFlightDetails, setCookie],
   );
 
-  return [parsedData, setFormCookie];
+  return useMemo(
+    () => [parsedData, setFormCookie],
+    [parsedData, setFormCookie],
+  );
 }
 
 export default useFormCookie;
