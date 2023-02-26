87bce712407db30166a1eaa8280bac7caf780ed5
Kirill Tchentsov <kirill.develops@gmail.com>
2023-02-26 14:16:50 -0500

    refactored to keep all state pertaining form in formik

--- components/StepperComponents/StepperStepButtons.jsx
+++ components/StepperComponents/StepperStepButtons.jsx
@@ -1,12 +1,11 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import React from 'react';
-import { transferSteps } from '@data/transferFormData';
 import { useFormikContext } from 'formik';
 
-function StepperStepButtons({ activeStep, handleStepChange }) {
-  const isLastStep = activeStep === transferSteps.length - 1;
-  const { submitForm } = useFormikContext();
+function StepperStepButtons({ handleBackClick, handleNextClick, isLastStep }) {
+  const { values, submitForm } = useFormikContext();
+  const { bookingStep } = values;
 
   return (
     <Stack
@@ -15,15 +14,15 @@ function StepperStepButtons({ activeStep, handleStepChange }) {
       justifyContent="flex-end"
     >
       <Button
-        onClick={() => handleStepChange(activeStep - 1)}
-        disabled={activeStep === 0}
+        onClick={() => handleBackClick(bookingStep - 1)}
+        disabled={bookingStep === 0}
         variant="outlined"
       >
         Prev
       </Button>
       <Button
         onClick={
-          isLastStep ? submitForm : () => handleStepChange(activeStep + 1)
+          isLastStep ? submitForm : () => handleNextClick(bookingStep + 1)
         }
         variant="contained"
       >
