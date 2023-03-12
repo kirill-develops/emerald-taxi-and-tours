52516ec27b30fc0eeebd434c42259ee004dadc7a
Kirill Tchentsov <kirill.develops@gmail.com>
2023-03-11 23:41:39 -0500

    refactored to parse correct cookie data

--- hooks/useFormCookie.jsx
+++ hooks/useFormCookie.jsx
@@ -10,34 +10,69 @@ function useFormCookie(initialValues) {
   const parseFlightDetails = useCallback(
     (data) => {
       const currentDateTime = dayjs();
-      const initialArrive = initialValues.flightDetails.arrive;
-      const initialDepart = initialValues.flightDetails.depart;
-      const arriveData = data?.flightDetails?.arrive;
-      const departData = data?.flightDetails?.depart;
+      const {
+        flightDetails: { arrive: initialArrive, depart: initialDepart },
+      } = initialValues;
+      const dataArrive = data?.flightDetails?.arrive;
+      const dataDepart = data?.flightDetails?.depart;
+
       const isArriveBeforeCurrentDate =
-        dayjs(arriveData).isBefore(currentDateTime);
+        dataArrive && dayjs(dataArrive).isBefore(currentDateTime);
       const isDepartBeforeCurrentDate =
-        dayjs(departData).isBefore(currentDateTime);
+        dataDepart && dayjs(dataDepart).isBefore(currentDateTime);
+
+      // console.table(
+      //   'currentDateTime: ',
+      //   currentDateTime,
+      //   'dataArrive: ',
+      //   dataArrive,
+      //   'isArriveBeforeCurrentDate: ',
+      //   isArriveBeforeCurrentDate,
+      //   'dataDepart: ',
+      //   dataDepart,
+      //   'isDepartBeforeCurrentDate: ',
+      //   isDepartBeforeCurrentDate,
+      // ); //! for debugging
 
       return {
         ...initialValues.flightDetails,
-        ...data.flightDetails,
-        arrive: isArriveBeforeCurrentDate ? initialArrive : dayjs(arriveData),
-        depart: isDepartBeforeCurrentDate ? initialDepart : dayjs(departData),
+        ...data?.flightDetails,
+        arrive: isArriveBeforeCurrentDate ? initialArrive : dayjs(dataArrive),
+        depart: isDepartBeforeCurrentDate ? initialDepart : dayjs(dataDepart),
       };
     },
-    [initialValues.flightDetails],
+    [initialValues],
   );
 
-  const parsedData = useMemo(() => {
-    if (!data) return initialValues;
-
+  const formattedCookie = useMemo(() => {
     return {
       ...initialValues,
       ...data,
       flightDetails: parseFlightDetails(data),
+      personalDetails: {
+        ...initialValues.personalDetails,
+        ...data?.personalDetails,
+      },
     };
   }, [data, initialValues, parseFlightDetails]);
+
+  const getUpdatedCookie = useCallback(
+    (values) => {
+      return {
+        ...data,
+        ...values,
+        flightDetails: {
+          ...data?.flightDetails,
+          ...values.flightDetails,
+        },
+        personalDetails: {
+          ...data?.personalDetails,
+          ...values.personalDetails,
+        },
+      };
+    },
+    [data],
+  );
 
   const setFormCookie = useCallback(
     (values) => {
@@ -46,26 +81,33 @@ function useFormCookie(initialValues) {
         maxAge: 60 * 60 * 24 * 7, // 1 week
       };
 
-      const formattedCookie = {
-        ...data,
-        flightDetails: parseFlightDetails(data),
-      };
-
       const areValuesSame = isEqual(formattedCookie, values);
 
+      // console.log(areValuesSame, formattedCookie, values); //! for debugging
+
       if (!areValuesSame) {
+        const updatedCookie = getUpdatedCookie(values);
+
+        // console.log(updatedCookie); //! for debugging
+
         setCookie(
           'EmeraldTransferFormCache',
-          JSON.stringify({
-            ...data,
-            ...values,
-          }),
+          JSON.stringify(updatedCookie),
           cookieOptions,
         );
       }
     },
-    [data, parseFlightDetails, setCookie],
+    [formattedCookie, setCookie, getUpdatedCookie],
   );
+
+  const parsedData = useMemo(() => {
+    if (!data) {
+      setFormCookie(initialValues);
+      return initialValues;
+    }
+
+    return formattedCookie;
+  }, [data, initialValues, formattedCookie, setFormCookie]);
 
   return useMemo(
     () => [parsedData, setFormCookie],
