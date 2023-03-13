d39bd7c29628e3b98d9314767e6e2fe84034c3e2
Kirill Tchentsov <kirill.develops@gmail.com>
2023-03-12 22:26:25 -0400

    added a loading transition to the navbar is it loads

--- src/components/NavbarComponents/NavbarLayout.jsx
+++ src/components/NavbarComponents/NavbarLayout.jsx
@@ -1,18 +1,18 @@
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import { useTheme } from '@mui/material/styles';
+import Image from 'next/image';
 import React from 'react';
 import Link from '@material/Link';
 import MobilePagesMenu from './MobilePagesMenu';
 import MobileSearchModal from './MobileSearchModal';
 import TabletPagesLink from './TabletPagesLink';
-import Image from 'next/image';
 import emeraldLogo from '@Public/logo/emerald_logo_v2.png';
-import Stack from '@mui/material/Stack';
+import useLoadingTransition from '@hooks/useLoadingTransition';
 
 const logoStyle = {
   aspectRatio: '1 / 1',
@@ -41,6 +41,8 @@ function ElevationScroll(props) {
 function NavbarLayout(props) {
   const menuBreakpoint = 'md';
 
+  const loadingTransitionStyle = useLoadingTransition();
+
   return (
     <ElevationScroll {...props}>
       <AppBar
@@ -49,6 +51,7 @@ function NavbarLayout(props) {
           top: { xs: 'auto', sm: '0' },
           bottom: { xs: 0, sm: 'initial' },
           backgroundColor: (theme) => theme.palette.background.default,
+          ...loadingTransitionStyle,
         }}
       >
         <Toolbar>
@@ -74,6 +77,7 @@ function NavbarLayout(props) {
                 height={42}
                 width={42}
                 style={logoStyle}
+                blurDataURL
               />
             </Link>
             <Typography
