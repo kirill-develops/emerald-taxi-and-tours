6858d38484aa9938bb21b14cde3ed6a86f450224
Kirill Tchentsov <kirill.develops@gmail.com>
2023-03-24 12:17:31 -0400

    refactored to be used in both tour and transfer forms

--- src/components/StepperComponents/StepperProgressBar.jsx
+++ src/components/StepperComponents/StepperProgressBar.jsx
@@ -1,21 +1,56 @@
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Stepper from '@mui/material/Stepper';
-import React from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
+import { ParamContext } from '@Form/FormContextProvider';
+import { tourSteps, transferSteps } from '@data/transferSteps';
 
-import { useTransferDataByKey } from '@data/transferFormData';
+export function useDataByKey(keys, data) {
+  if (!Array.isArray(keys)) {
+    keys = [keys];
+  }
+
+  return useMemo(
+    () =>
+      data.map((data) =>
+        keys.reduce((accumulator, key) => {
+          return {
+            ...accumulator,
+            [key]: data[key]?.toString ? data[key].toString() : data[key],
+          };
+        }, []),
+      ),
+    [data, keys],
+  );
+}
 
 function StepperProgressBar({ activeStep }) {
-  const transferLabels = useTransferDataByKey('label');
+  const context = useContext(ParamContext);
+
+  const stepsData = useMemo(
+    () => (context.type === 'transfer' ? transferSteps : tourSteps),
+    [context.type],
+  );
+
+  const labels = useDataByKey('label', stepsData);
+
+  const getLabel = useCallback(
+    (index, text) => {
+      if (activeStep >= index) {
+        return text;
+      }
+    },
+    [activeStep],
+  );
 
   return (
     <Stepper
       activeStep={activeStep}
       alternativeLabel
     >
-      {transferLabels.map(({ label }, index) => (
+      {labels.map(({ label }, index) => (
         <Step key={label}>
-          <StepLabel>{activeStep >= index && label}</StepLabel>
+          <StepLabel>{getLabel(index, label)}</StepLabel>
         </Step>
       ))}
     </Stepper>
