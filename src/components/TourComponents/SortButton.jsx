7278394d55254a8dc8189e629a603b99cab74585
Kirill Tchentsov <kirill.develops@gmail.com>
2023-05-14 00:31:52 -0400

    refactored to show tooltip on disabled

--- src/components/TourComponents/SortButton.jsx
+++ src/components/TourComponents/SortButton.jsx
@@ -3,30 +3,48 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import React, { useMemo } from 'react';
-import { useTour } from '@state/useTour';
+import React, { useCallback, useMemo } from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import Zoom from '@mui/material/Zoom';
 
-function SortTooltip({ title = '', children, sx, disabled = false, ...rest }) {
-  return (
-    <Tooltip
-      title={title}
-      arrow
-      followCursor
-      TransitionComponent={Zoom}
-      placement="left"
-      disableFocusListener={disabled}
-      disableHoverListener={disabled}
-      disableInteractive={disabled}
-      disableTouchListener={disabled}
-      sx={{ ...sx }}
-      {...rest}
-    >
-      <span>{children}</span>
-    </Tooltip>
-  );
-}
+import { useTour } from '@state/useTour';
+
+const formControlStyles = {
+  width: { xs: '40%', sm: '28%' },
+  minWidth: 195,
+  maxWidth: 225,
+};
+
+const inputLabelStyles = {
+  width: '45%',
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const sortOutlinedStyles = { ml: 1 };
+
+const SortTooltip = React.memo(
+  ({ title = '', children, sx, disabled = false, ...rest }) => {
+    return (
+      <Tooltip
+        title={title}
+        arrow
+        followCursor
+        TransitionComponent={Zoom}
+        placement="left"
+        disableFocusListener={disabled}
+        disableHoverListener={disabled}
+        disableInteractive={disabled}
+        disableTouchListener={disabled}
+        sx={{ ...sx }}
+        {...rest}
+      >
+        <span>{children}</span>
+      </Tooltip>
+    );
+  },
+);
+SortTooltip.displayName = 'SortTooltip';
 
 function SortButton() {
   const [state, actions] = useTour();
@@ -38,43 +56,25 @@ function SortButton() {
     [filterStartLocation],
   );
 
-  const handleSortChange = (e) => {
-    setSort(e.target.value);
-  };
+  const handleSortChange = useCallback(
+    (e) => {
+      setSort(e.target.value);
+    },
+    [setSort],
+  );
 
-  const handleClearSelect = (e) => {
-    if (e.keyCode === 27) {
-      setSort('');
-    }
-  };
+  const handleClearSelect = useCallback(
+    (e) => {
+      if (e.keyCode === 27) {
+        setSort('');
+      }
+    },
+    [setSort],
+  );
 
-  return (
-    <FormControl
-      onKeyUp={handleClearSelect}
-      sx={{
-        width: { xs: '40%', sm: '28%' },
-        minWidth: 195,
-        maxWidth: 225,
-      }}
-      size={'small'}
-    >
-      <InputLabel
-        sx={{
-          width: '45%',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        Sort
-        <SortOutlined sx={{ ml: 1 }} />
-      </InputLabel>
-      <Select
-        label="Sort......."
-        value={sort}
-        onChange={handleSortChange}
-      >
-        <MenuItem value="alphabetical"> Alphabetical</MenuItem>
-
+  const priceAscendingJSX = useMemo(
+    () =>
+      hasStartLocation ? (
         <MenuItem
           value="priceAscending"
           disabled={!hasStartLocation}
@@ -86,7 +86,25 @@ function SortButton() {
             Price: Low to High
           </SortTooltip>
         </MenuItem>
+      ) : (
+        <SortTooltip
+          title="select starting location for price sort"
+          disabled={hasStartLocation}
+        >
+          <MenuItem
+            value="priceAscending"
+            disabled={!hasStartLocation}
+          >
+            Price: Low to High
+          </MenuItem>
+        </SortTooltip>
+      ),
+    [hasStartLocation],
+  );
 
+  const priceDescendingJSX = useMemo(
+    () =>
+      hasStartLocation ? (
         <MenuItem
           value="priceDescending"
           disabled={!hasStartLocation}
@@ -98,10 +116,44 @@ function SortButton() {
             Price: High to Low
           </SortTooltip>
         </MenuItem>
+      ) : (
+        <SortTooltip
+          title="select starting location for price sort"
+          disabled={hasStartLocation}
+        >
+          <MenuItem
+            value="priceDescending"
+            disabled={!hasStartLocation}
+          >
+            Price: High to Low
+          </MenuItem>
+        </SortTooltip>
+      ),
+    [hasStartLocation],
+  );
+
+  return (
+    <FormControl
+      onKeyUp={handleClearSelect}
+      sx={formControlStyles}
+      size={'small'}
+    >
+      <InputLabel sx={inputLabelStyles}>
+        Sort
+        <SortOutlined sx={sortOutlinedStyles} />
+      </InputLabel>
+      <Select
+        label="Sort......."
+        value={sort}
+        onChange={handleSortChange}
+      >
+        <MenuItem value="alphabetical"> Alphabetical</MenuItem>
+        {priceAscendingJSX}
+        {priceDescendingJSX}
         <MenuItem value="region">Region: Alphabetical</MenuItem>
       </Select>
     </FormControl>
   );
 }
 
-export default SortButton;
+export default React.memo(SortButton);
