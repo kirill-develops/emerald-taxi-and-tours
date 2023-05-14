fc2b8e4ad03204c51cf39118e5c6ac872661c256
Kirill Tchentsov <kirill.develops@gmail.com>
2023-05-13 22:39:37 -0400

    added ToursByType to TourGrid, optimized and create All Tour Header

--- src/components/TourComponents/TourGrid.jsx
+++ src/components/TourComponents/TourGrid.jsx
@@ -1,31 +1,64 @@
 import Container from '@mui/material/Container';
-import React from 'react';
-import { gridSpacingProps } from '@material/theme';
+import React, { useMemo } from 'react';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
 import { useTour } from '@state/useTour';
 import TourCard from './TourCard';
 import { GridContainer } from '@elements/CustomGridEl';
 import useSortData from '@hooks/useSortData';
+import { GridItem } from '@elements/CustomGridEl';
+import ToursByType from './ToursByType';
+import { gridSpacingProps } from '@material/theme';
+import TourOptions from './TourOptions';
 
-function TourGrid() {
+function AllToursHeader() {
+  return useMemo(
+    () => (
+      <GridItem sx={{ width: '100%' }}>
+        <Typography variant="h4">All Tours</Typography>
+        <Divider
+          variant="inset"
+          textAlign="right"
+          sx={{ marginBottom: 3 }}
+        />
+        <TourOptions />
+      </GridItem>
+    ),
+    [],
+  );
+}
+
+function SortedTours() {
   const [state, actions] = useTour();
   const { sort, filteredData, filterStartLocation } = state;
 
   const sortedTourData = useSortData(filteredData, sort, filterStartLocation);
 
+  return useMemo(
+    () =>
+      sortedTourData.map((tour) => (
+        <TourCard
+          tour={tour}
+          key={tour.link}
+        />
+      )),
+    [sortedTourData],
+  );
+}
+
+function TourGrid() {
   return (
     <Container
       maxWidth="lg"
       sx={{ pl: { sm: 0 }, pr: { sm: 1 } }}
     >
       <GridContainer spacing={gridSpacingProps}>
-        {sortedTourData.map((tour) => (
-          <TourCard
-            tour={tour}
-            key={tour.link}
-          />
-        ))}
+        <ToursByType />
+        <AllToursHeader />
+        <SortedTours />
       </GridContainer>
     </Container>
   );
 }
-export default TourGrid;
+export default React.memo(TourGrid);
