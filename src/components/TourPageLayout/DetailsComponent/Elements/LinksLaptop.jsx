ffc5258a6fb221426133431b6b1d79f43fab085c
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-16 22:11:09 -0400

    added conditional checking & email link

--- src/components/TourPageLayout/DetailsComponent/Elements/LinksLaptop.jsx
+++ src/components/TourPageLayout/DetailsComponent/Elements/LinksLaptop.jsx
@@ -1,17 +1,23 @@
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { Laptop, Launch, Phone } from '@mui/icons-material';
+import { Laptop, Launch, MailOutline, Phone, Place } from '@mui/icons-material';
 import React, { useContext } from 'react';
 import { PopUpLink, TelLink } from '@elements/Links';
 import DetailDevider from './DetailDevider';
 import { ParamContext } from '@Form/FormContextProvider';
 import { detailTypographyProps } from '..';
+import { styled } from '@mui/material';
+
+const AddressText = styled(Typography)(({ theme }) =>
+  theme.unstable_sx({ display: 'flex', alignItems: 'center', gap: 0.5 }),
+);
 
 export default React.memo(function LinksLaptop() {
   const {
     tourParams: {
       tripAdvisorDetails: {
         address_obj: { address_string: addressString = '' } = {},
+        email,
         phone,
         web_url: webUrl,
         website,
@@ -26,20 +32,35 @@ export default React.memo(function LinksLaptop() {
         spacing={2}
         divider={<DetailDevider />}
       >
-        <PopUpLink href={website}>
-          <Laptop fontSize="small" />
-          Website
-        </PopUpLink>
-        <TelLink href={`tel:${phone}`}>
-          <Phone fontSize="small" />
-          {phone}
-        </TelLink>
-        <PopUpLink href={webUrl}>
-          <Launch fontSize="small" />
-          TripAdvisor Page
-        </PopUpLink>
+        {website && (
+          <PopUpLink href={website}>
+            <Laptop fontSize="small" />
+            Website
+          </PopUpLink>
+        )}
+        {phone && (
+          <TelLink href={phone}>
+            <Phone fontSize="small" />
+            {phone}
+          </TelLink>
+        )}
+        {webUrl && (
+          <PopUpLink href={webUrl}>
+            <Launch fontSize="small" />
+            TripAdvisor Page
+          </PopUpLink>
+        )}
+        {email && (
+          <TelLink href={email}>
+            <MailOutline fontSize="small" />
+            Email
+          </TelLink>
+        )}
       </Stack>
-      <Typography {...detailTypographyProps}>{addressString}</Typography>
+      <AddressText {...detailTypographyProps}>
+        <Place fontSize="small" />
+        {addressString}
+      </AddressText>
     </>
   );
 });
