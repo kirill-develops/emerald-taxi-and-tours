04aa4375ac95de3f80731427427615336eb2e00e
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-19 19:48:36 -0400

    corrected AboutLayout bug

--- src/components/AboutLayout/index.jsx
+++ src/components/AboutLayout/index.jsx
@@ -3,11 +3,6 @@ import FormInputStack from '@elements/FormInputStack';
 import PageCard from '@elements/PageCard';
 import Summary from './elements/Summary';
 import AboutImage from './elements/AboutImage';
-import { styled } from '@mui/material/styles';
-
-const AboutStack = styled(FormInputStack)(({ theme }) =>
-  theme.unstable_sx({ columnGap: 0, rowGap: 0 }),
-);
 
 const AboutCard = React.memo(function AboutCard(props) {
   return (
@@ -21,9 +16,11 @@ const AboutCard = React.memo(function AboutCard(props) {
 
 export default React.memo(function AboutLayout() {
   return (
-    <AboutStack component={AboutCard}>
-      <AboutImage />
-      <Summary />
-    </AboutStack>
+    <AboutCard>
+      <FormInputStack>
+        <AboutImage />
+        <Summary />
+      </FormInputStack>
+    </AboutCard>
   );
 });
