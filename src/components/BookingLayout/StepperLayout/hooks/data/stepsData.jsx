dfec1462f64707ed44af6d0e7bea172c9e051cb3
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-22 11:29:06 -0400

    moved data to seperate file

---
+++ src/components/BookingLayout/StepperLayout/hooks/data/stepsData.jsx
@@ -0,0 +1,51 @@
+import FormFlightDetails from '@Form/FormFlightDetails';
+import FormTourDetails from '@Form/FormTourDetails';
+import FormPersonalDetails from '@Form/FormPersonalDetails';
+import FormPaymentDetails from '@Form/FormPaymentDetails';
+import FormConfirmDetails from '@Form/FormConfirmDetails';
+
+export const transferSteps = [
+  {
+    label: 'Flight Details',
+    link: 'flightDetails',
+    component: <FormFlightDetails />,
+  },
+  {
+    label: 'Personal Details',
+    link: 'personalDetails',
+    component: <FormPersonalDetails />,
+  },
+  {
+    label: 'Payment Options',
+    link: 'paymentDetails',
+    component: <FormPaymentDetails />,
+  },
+  {
+    label: 'Confirm',
+    link: 'confirmDetails',
+    component: <FormConfirmDetails />,
+  },
+];
+
+export const tourSteps = [
+  {
+    label: 'Tour Details',
+    link: 'tourDetails',
+    component: <FormTourDetails />,
+  },
+  {
+    label: 'Personal Details',
+    link: 'personalDetails',
+    component: <FormPersonalDetails />,
+  },
+  {
+    label: 'Payment Options',
+    link: 'paymentDetails',
+    component: <FormPaymentDetails />,
+  },
+  {
+    label: 'Confirm',
+    link: 'confirmDetails',
+    component: <FormConfirmDetails />,
+  },
+];
--- src/components/BookingLayout/StepperLayout/hooks/useStepperData.jsx
+++ src/components/BookingLayout/StepperLayout/hooks/useStepperData.jsx
@@ -1,57 +1,7 @@
 import { useContext, useMemo } from 'react';
-import FormFlightDetails from '@Form/FormFlightDetails';
-import FormTourDetails from '@Form/FormTourDetails';
-import FormPersonalDetails from '@Form/FormPersonalDetails';
-import FormPaymentDetails from '@Form/FormPaymentDetails';
-import FormConfirmDetails from '@Form/FormConfirmDetails';
 import { ParamContext } from '@Form/FormContextProvider';
 import useDataByKey from '@hooks/useDataByKey';
-
-export const transferSteps = [
-  {
-    label: 'Flight Details',
-    link: 'flightDetails',
-    component: <FormFlightDetails />,
-  },
-  {
-    label: 'Personal Details',
-    link: 'personalDetails',
-    component: <FormPersonalDetails />,
-  },
-  {
-    label: 'Payment Options',
-    link: 'paymentDetails',
-    component: <FormPaymentDetails />,
-  },
-  {
-    label: 'Confirm',
-    link: 'confirmDetails',
-    component: <FormConfirmDetails />,
-  },
-];
-
-export const tourSteps = [
-  {
-    label: 'Tour Details',
-    link: 'tourDetails',
-    component: <FormTourDetails />,
-  },
-  {
-    label: 'Personal Details',
-    link: 'personalDetails',
-    component: <FormPersonalDetails />,
-  },
-  {
-    label: 'Payment Options',
-    link: 'paymentDetails',
-    component: <FormPaymentDetails />,
-  },
-  {
-    label: 'Confirm',
-    link: 'confirmDetails',
-    component: <FormConfirmDetails />,
-  },
-];
+import { tourSteps, transferSteps } from './data/stepsData';
 
 export default function useStepperData(bookingStep = 0) {
   const context = useContext(ParamContext);
