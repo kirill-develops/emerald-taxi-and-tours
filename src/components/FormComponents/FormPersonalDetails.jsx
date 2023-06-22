8d3cb60b42bc6dbfb64980ba586b88439dac7792
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-22 17:04:37 -0400

    refactored to avoid rerenders

--- src/components/FormComponents/FormPersonalDetails.jsx
+++ src/components/FormComponents/FormPersonalDetails.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import FormInputStack from '@elements/FormInputStack';
 import FormTextField from '@elements/FormTextField';
-import FormPhoneField from '@Form/FormPhoneField';
+import FormPhoneField from '@Form/PhoneField/PhoneField';
+
+const lastNameFieldStyles = { mt: { xxs: 1, sm: 0 } };
+
+const inputStackStyles = { width: '100%' };
 
 function FormPersonalDetails() {
   const stepName = 'personalDetails';
 
   return (
     <>
-      <FormInputStack sx={{ width: '100%' }}>
+      <FormInputStack sx={inputStackStyles}>
         <FormTextField
           stepName={stepName}
           fieldName="firstName"
@@ -18,7 +22,7 @@ function FormPersonalDetails() {
           stepName={stepName}
           fieldName="lastName"
           label="Last Name"
-          sx={{ mt: { xxs: 1, sm: 0 } }}
+          sx={lastNameFieldStyles}
         />
       </FormInputStack>
       <FormTextField
