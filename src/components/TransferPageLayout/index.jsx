8d5fab210c9d12bcf992e322bf7df8be01dc6bec
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-24 16:17:21 -0400

    added PageCard styling

--- src/components/TransferPageLayout/index.jsx
+++ src/components/TransferPageLayout/index.jsx
@@ -1,17 +1,27 @@
 import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 import BookingLayout from '@components/BookingLayout/BookingLayout';
 import ImportantInfo from '../TourPageLayout/ImportantInfo';
 import TransferDescription from './Elements/TransferDescription';
 import TransferTitle from './Elements/TransferTitle';
+import PageCard from '@elements/PageCard';
 
 const transferStackStyles = { mt: 4 };
+
+const DescriptionCard = styled(PageCard)(({ theme }) =>
+  theme.unstable_sx({
+    py: { xxs: 2, md: 4.5 },
+    position: 'relative',
+  }),
+);
 
 function TransferStack({ children, ...rest }) {
   return (
     <Stack
       spacing={2}
       sx={transferStackStyles}
+      {...rest}
     >
       {children}
     </Stack>
@@ -23,8 +33,10 @@ export default function TransferPageLayout() {
     <>
       <BookingLayout />
       <TransferStack>
-        <TransferTitle />
-        <TransferDescription />
+        <DescriptionCard>
+          <TransferTitle />
+          <TransferDescription />
+        </DescriptionCard>
         <ImportantInfo />
       </TransferStack>
     </>
