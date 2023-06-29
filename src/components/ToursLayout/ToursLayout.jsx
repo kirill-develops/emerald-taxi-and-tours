53c1b808fed3ec17608fb9b7ae1ea2d2c24c1815
Kirill Tchentsov <kirill.develops@gmail.com>
2023-06-28 20:19:14 -0400

    refactored for verbositiy

--- src/components/ToursLayout/ToursLayout.jsx
+++ src/components/ToursLayout/ToursLayout.jsx
@@ -4,32 +4,54 @@ import TourOptions from './TourOptions/TourOptions';
 import FilterOptions from './FilterOptions/FilterOptions';
 import TourGrid from './TourGrid/TourGrid';
 import { GridContainer, GridItem } from '@elements/CustomGridEl';
-
-const tourOptionsStyles = { display: { xxs: 'none', sm: 'block' } };
+import { styled } from '@mui/material';
 
 const gridItemStyles = { display: { xxs: 'none', sm: 'block' } };
+
+const TabletTourOptions = styled(TourOptions)(({ theme }) =>
+  theme.unstable_sx({ display: { xxs: 'none', sm: 'block' } }),
+);
+
+function Aside({ children, sx, ...rest }) {
+  return (
+    <GridItem
+      sm={4}
+      md={3}
+      lg={2.5}
+      sx={{ ...gridItemStyles, ...sx }}
+      {...rest}
+    >
+      {children}
+    </GridItem>
+  );
+}
+
+function Content({ children, sx, ...rest }) {
+  return (
+    <GridItem
+      xxs={12}
+      sm={8}
+      md={9}
+      lg={9.5}
+      sx={sx}
+      {...rest}
+    >
+      {children}
+    </GridItem>
+  );
+}
 
 export default React.memo(function TourLayout() {
   return (
     <>
-      <TourOptions sx={tourOptionsStyles} />
+      <TabletTourOptions />
       <GridContainer spacing={gridSpacingProps}>
-        <GridItem
-          sm={4}
-          md={3}
-          lg={2.5}
-          sx={gridItemStyles}
-        >
+        <Aside>
           <FilterOptions />
-        </GridItem>
-        <GridItem
-          xxs={12}
-          sm={8}
-          md={9}
-          lg={9.5}
-        >
+        </Aside>
+        <Content>
           <TourGrid />
-        </GridItem>
+        </Content>
       </GridContainer>
     </>
   );
