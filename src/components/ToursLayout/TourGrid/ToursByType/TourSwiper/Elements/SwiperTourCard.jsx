0b16744df08d7b19fa4baac3a60a59ef39223d33
Kirill Tchentsov <kirill.develops@gmail.com>
2023-07-17 20:33:41 -0400

    extended details in subheader

--- src/components/ToursLayout/TourGrid/ToursByType/TourSwiper/Elements/SwiperTourCard.jsx
+++ src/components/ToursLayout/TourGrid/ToursByType/TourSwiper/Elements/SwiperTourCard.jsx
@@ -2,30 +2,62 @@ import React, { useMemo } from 'react';
 import GridCard from '@components/GridCard/GridCard';
 import { getToursUrl } from '@pages/tours/[area]/[tour]';
 
+function getSubcategoryString(subcategory) {
+  return subcategory
+    ?.slice(0, 3)
+    .map(({ localized_name }) => localized_name)
+    .join(', ');
+}
+
 export default function SwiperTourCard({ tour, sx, ...rest }) {
-  const { name, area, link, price, areaLink, tripAdvisorPhotos } = tour;
+  const {
+    name,
+    area,
+    link,
+    price,
+    areaLink,
+    tripAdvisorPhotos,
+    tripAdvisorDetails: {
+      cuisine,
+      groups,
+      price_level: priceLevel,
+      subcategory,
+    } = {},
+  } = tour;
 
-  const areaPrice = price.reduce((acc, priceObj) =>
-    priceObj.price < acc ? (acc = priceObj.price) : acc,
+  const areaPrice = price.reduce(
+    (acc, priceObj) => (priceObj.price < acc ? priceObj.price : acc),
+    price[0].price,
   );
 
-  const url = getToursUrl(areaLink, link);
+  const url = useMemo(() => getToursUrl(areaLink, link), [areaLink, link]);
 
-  const cardStyles = useMemo(
-    () => ({ height: { xxs: 155, sm: 190 }, ...sx }),
-    [sx],
+  const cardStyles = useMemo(() => ({ height: { xxs: 155 }, ...sx }), [sx]);
+
+  const tripAdvisorSubcategory = useMemo(
+    () => cuisine ?? groups ?? subcategory,
+    [cuisine, groups, subcategory],
   );
+
+  const subcategoryString = useMemo(
+    () => getSubcategoryString(tripAdvisorSubcategory),
+    [tripAdvisorSubcategory],
+  );
+
+  const subheading = `${area} | ${priceLevel ? `${priceLevel} | ` : ''}${
+    subcategoryString || ''
+  }`;
 
   return (
     <GridCard
       disableRipple
       noWrap
       noGrid
-      price={areaPrice.price}
+      price={areaPrice}
       picData={tripAdvisorPhotos[0]}
       bookNowUrl={url}
       name={name}
-      area={area}
+      area={subheading}
       sx={cardStyles}
       {...rest}
     />
