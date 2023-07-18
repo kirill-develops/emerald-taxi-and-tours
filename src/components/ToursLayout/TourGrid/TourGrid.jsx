300398344a55d95d52f72ae035e257d35a69d95b
Kirill Tchentsov <kirill.develops@gmail.com>
2023-07-18 12:25:23 -0400

    added FeaturedToursByLocation to TourGrid

--- src/components/ToursLayout/TourGrid/TourGrid.jsx
+++ src/components/ToursLayout/TourGrid/TourGrid.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MaxWidthContainer from '@elements/MaxWidthContainer';
-import { GridContainer } from '@elements/CustomGridEl';
+import { GridContainer, GridItem } from '@elements/CustomGridEl';
 import ToursByType from './ToursByType/ToursByType';
 import { layoutGridSpacingProp } from '@elements/CustomGridEl';
 import AllTours from './AllTours/AllTours';
+import FeaturedToursByLocation from '@components/HomeLayout/Elements/FeaturedToursByLocation';
 
 const gridContainerStyles = { pl: { sm: 0 }, m: 0 };
 
@@ -16,6 +17,9 @@ export default React.memo(function TourGrid() {
     >
       <GridContainer spacing={layoutGridSpacingProp}>
         <ToursByType />
+        <GridItem>
+          <FeaturedToursByLocation />
+        </GridItem>
         <AllTours />
       </GridContainer>
     </MaxWidthContainer>
