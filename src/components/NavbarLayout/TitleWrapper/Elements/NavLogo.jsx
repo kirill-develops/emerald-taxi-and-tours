aa135e2b3a28654e4c33ea0af0d5ed7badc699c9
Kirill Tchentsov <kirill.develops@gmail.com>
2023-10-14 16:10:33 -0400

    added sharp for production image optimization

--- src/components/NavbarLayout/TitleWrapper/Elements/NavLogo.jsx
+++ src/components/NavbarLayout/TitleWrapper/Elements/NavLogo.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import React from 'react';
 
 import Link from '@material/Link';
-import emeraldLogo from '@../public/logo/emerald_logo_v2.PNG';
 import { styled } from '@mui/material';
 
 const LogoLink = styled(Link)(({ theme }) =>
@@ -13,9 +12,10 @@ export default function NavLogo() {
   return (
     <LogoLink href="/">
       <Image
-        src={emeraldLogo}
+        src="/logo/emerald_logo_v2.png"
         alt="Emerald Taxi & Tour Logo"
         width={42}
+        height={42}
         quality={100}
         blurDataURL
       />
