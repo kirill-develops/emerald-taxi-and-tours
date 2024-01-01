a8149a2e6e8f0d9039836d511b0db62a2662dc87
Kirill Tchentsov <kirill.develops@gmail.com>
2024-01-01 11:11:52 -0500

    updated pricing overlay styling

--- src/components/GridCard/ImageOverlayWrapper/ImageOverlayWrapper.jsx
+++ src/components/GridCard/ImageOverlayWrapper/ImageOverlayWrapper.jsx
@@ -32,14 +32,111 @@ const OverlayItem2 = styled(Box)(({ theme }) =>
     ...commonOverlayStyles,
     top: 11,
     left: 11,
+    px: 0.75,
+    py: 0.5,
     borderRadius: 1,
     display: 'flex',
     flexDirection: 'column',
+    rowGap: 0,
     alignItems: 'center',
-    color: 'silver',
-    backgroundColor: 'rgba(0,0,0,0.65)',
-    px: 0.75,
-    py: 0.5,
+    boxShadow: 0,
+  }),
+);
+
+const PricingCaption = styled(Typography)(({ theme }) =>
+  theme.unstable_sx({
+    typography: 'body1',
+    fontSize: '0.7875rem',
+    fontStyle: 'italic',
+    fontWeight: 200,
+    fontVariant: 'small-caps',
+    letterSpacing: '-0.00688rem',
+    lineHeight: 0.85,
+    color: 'white',
+  }),
+);
+
+const PricingFont = styled(Typography)(({ theme }) =>
+  theme.unstable_sx({
+    typography: 'body1',
+    fontSize: '1.2rem',
+    fontWeight: 800,
+    fontVariant: 'small-caps',
+    letterSpacing: '-0.02938rem',
+    lineHeight: 0.85,
+    color: '#ffb952',
+    // WebkitTextStroke: `0.1px ${theme.palette.secondary.main}`,
+  }),
+);
+
+const Elipse = styled((props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="58"
+    height="42"
+    viewBox="0 0 58 42"
+    fill="none"
+    {...props}
+  >
+    <g filter="url(#filter0_d_15_3376)">
+      <ellipse
+        cx="29"
+        cy="17"
+        rx="25"
+        ry="17"
+        fill="#3A4C2A"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_15_3376"
+        x="0"
+        y="0"
+        width="58"
+        height="42"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood
+          flood-opacity="0"
+          result="BackgroundImageFix"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite
+          in2="hardAlpha"
+          operator="out"
+        />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_15_3376"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_15_3376"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+))(({ theme }) =>
+  theme.unstable_sx({
+    position: 'absolute',
+    top: -1,
+    zIndex: -1,
+    fill: theme.palette.primary.container,
   }),
 );
 
@@ -71,8 +168,9 @@ export default React.memo(function ImageOverlayWrapper({
       </OverlayItem1>
       {price && (
         <OverlayItem2>
-          <Typography variant="smallCaption">FROM</Typography>
-          <Typography variant="smallBold">${price}</Typography>
+          <Elipse />
+          <PricingCaption>FROM</PricingCaption>
+          <PricingFont>${price}</PricingFont>
         </OverlayItem2>
       )}
       {awards?.[0]?.images?.large && (
